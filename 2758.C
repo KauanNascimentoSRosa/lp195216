ea94d2df9461c2408c0c7ec298098995f6331d10
KauanNascimentoSRosa <kauan.nascimento@unifesp.br>
2026-04-19 01:20:35 -0300

    2758.C

--- 2758.C
+++ 2758.C
@@ -1,3 +1,13 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Kauan Nascimento Souza Rosa
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/2758
+Data        : 14/04/2026
+Objetivo    : Escrever dois números de precisão simples e dois de dupla com 6, 1, 2 e 3 algarismos após a vírgula, indicação de quantas multiplicações por 10 são necessárias após o primeiro algarismo e sem os números após a vírgula.
+Aprendizado : Modificações para o float e double para a impressão de número.
+-------------------------------------------------------------------------- */
+
 #include <stdio.h>
 
 int main()
