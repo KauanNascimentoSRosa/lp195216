dd6dfeee3990e099a1439a71c8dfeac25d52bc45
KauanNascimentoSRosa <kauan.nascimento@unifesp.br>
2026-04-19 01:03:31 -0300

    1000.C

--- 1000.C
+++ 1000.C
@@ -1,3 +1,13 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Kauan Nascimento Souza Rosa
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1071
+Data        : 11/04/2026
+Objetivo    : Imprimir "Hello World!".
+Aprendizado : Uso do comando "printf" e suas exigências.
+-------------------------------------------------------------------------- */
+
 #include <stdio.h>
 
 int main()
