7c54ef5b454e7ac5294ef19de80e0d6cd82d25a9
KauanNascimentoSRosa <kauan.nascimento@unifesp.br>
2026-04-21 14:37:05 -0300

    1080.b.C

--- 1080.b.C
+++ 1080.b.C
@@ -1,3 +1,13 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Kauan Nascimento Souza Rosa
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1080
+Data        : 21/04/2026
+Objetivo    : Ler 100 números, indicar o maior e sua posição utilizando vetor.
+Aprendizado : Como usar vetor para ler números, indicar o maior e a posição dele em conjunto com o "for". 
+-------------------------------------------------------------------------- */
+
 include <stdio.h>
 #include <stdlib.h>
 
