7435c5087089dce805b9f1e29135118c6b6a51f4
KauanNascimentoSRosa <kauan.nascimento@unifesp.br>
2026-05-03 20:42:10 -0300

    1253.C

--- 1253.C
+++ 1253.C
@@ -1,3 +1,13 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Kauan Nascimento Souza Rosa
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1253
+Data        : 03/05/2026
+Objetivo    : Decifrar um código modificando as letras de acordo com a sua posição no alfabeto.
+Aprendizado : Utilização de fgets e getchar para ler as strings do código não decifrado e retornar uma string  com o código decifrado. 
+-------------------------------------------------------------------------- */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
