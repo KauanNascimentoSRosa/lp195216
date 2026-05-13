d98e9a4dd40c7fa5b92b78efeba9771ff0f1e5f4
KauanNascimentoSRosa <kauan.nascimento@unifesp.br>
2026-05-12 21:44:25 -0300

    1534.a.C

--- 1534.a.C
+++ 1534.a.C
@@ -1,3 +1,13 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Kauan Nascimento Souza Rosa
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1534
+Data        : 12/05/2026
+Objetivo    : Criar uma matriz, de tamanho NxN, em que a diagonal da esquerda para a direita possui o número 1, a outra diagonal possui o número 2, e os demais lugares o número 3.
+Aprendizado : Criação de matriz e inserção de números em cada espaço com auxílio do for. 
+-------------------------------------------------------------------------- */
+
 #include <stdio.h>
 #include <stdlib.h>
 
