da8a005f7d519daf1b1b6de5d0bef3cc01820936
KauanNascimentoSRosa <kauan.nascimento@unifesp.br>
2026-05-19 22:28:37 -0300

    1017.C

--- 1017.C
+++ 1017.C
@@ -1,3 +1,13 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Kauan Nascimento Souza Rosa
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1017
+Data        : 19/05/2026
+Objetivo    : Calcular a quantidade de litros que serão gastos em uma viagem em função da velocidade e do tempo.
+Aprendizado : Utilização de função para expressar uma conta.
+-------------------------------------------------------------------------- */
+
 #include <stdio.h>
 
 float calcularLitros(int tempo, int velocidade){
