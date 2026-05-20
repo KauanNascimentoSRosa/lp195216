8cd6a8a47a7d26c76abebc7c207763c1375589f2
KauanNascimentoSRosa <kauan.nascimento@unifesp.br>
2026-05-19 22:39:00 -0300

    1046.C

--- 1046.C
+++ 1046.C
@@ -1,3 +1,13 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Kauan Nascimento Souza Rosa
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1046
+Data        : 19/05/2026
+Objetivo    : Calcular a duração de um jogo com base na hora inicial e final.
+Aprendizado : Utilização de função para calcular o tempo e uso de printf para escrever uma frase.
+-------------------------------------------------------------------------- */
+
 #include <stdio.h>
 
 int calcularDuracao(int inicio, int fim){
