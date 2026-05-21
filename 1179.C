fd80419366dd785dd0bafa48d22422405d9da8bc
KauanNascimentoSRosa <kauan.nascimento@unifesp.br>
2026-05-21 12:21:09 -0300

    1179.C

--- 1179.C
+++ 1179.C
@@ -1,3 +1,13 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Kauan Nascimento Souza Rosa
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1179
+Data        : 21/05/2026
+Objetivo    : Ler 15 números, indicar os primeiros cinco pares, os cinco ímpares em sequência, e os que restaram, sendo primeiro os ímpares.
+Aprendizado : Utilização de variável para resetar o contador e reutilizar o vetor.
+-------------------------------------------------------------------------- */
+
 #include <stdio.h>
 
 int main()
