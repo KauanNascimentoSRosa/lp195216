e5d1e71ec39326f3e158eaf2b0976083834dde8b
KauanNascimentoSRosa <kauan.nascimento@unifesp.br>
2026-05-28 15:35:53 -0300

    1169.C

--- 1169.C
+++ 1169.C
@@ -1,3 +1,13 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Kauan Nascimento Souza Rosa
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1169
+Data        : 28/05/2026
+Objetivo    : Descobrir quantos kg de grão de trigo a rainha terá que dar para o monge de acordo com o número de casas to tabuleiro.
+Aprendizado : Usar recursividade para saber quantos trigos possui a última casa.
+-------------------------------------------------------------------------- */
+
 #include <stdio.h>
 
 unsigned long long cont(int n){
