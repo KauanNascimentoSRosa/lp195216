2adf0ca808e3bf1663fcf55883ff77414c602133
KauanNascimentoSRosa <kauan.nascimento@unifesp.br>
2026-06-01 19:02:32 -0300

    1028.C

--- 1028.C
+++ 1028.C
@@ -1,3 +1,13 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Kauan Nascimento Souza Rosa
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1028
+Data        : 01/06/2026
+Objetivo    : Calcular o mínimo divisor comum dos números de figurinhas.
+Aprendizado : Recursão para calcular o mdc.
+-------------------------------------------------------------------------- */
+
 int mdc(int F1, int F2){
     if (F2 == 0){
         return F1;
