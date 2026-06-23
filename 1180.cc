b2607c1317435a408a5162a16eb6a4863dd81784
KauanNascimentoSRosa <kauan.nascimento@unifesp.br>
2026-06-22 23:04:22 -0300

    1180.cc

--- 1180.cc
+++ 1180.cc
@@ -1,3 +1,13 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Kauan Nascimento Souza Rosa
+Linguagem   : C++
+Problema    : https://judge.beecrowd.com/pt/problems/view/1180
+Data        : 22/06/2026
+Objetivo    : Ler os números de um vetor, mostrar ele e sua posição.
+Aprendizado : Utilização de variável para localizar a posição do menor.
+-------------------------------------------------------------------------- */
+
 #include <iostream>
 using namespace std;
 
