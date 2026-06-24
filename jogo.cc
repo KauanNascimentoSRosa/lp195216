c4357af4ad2a7c893115694b4364ba06a5c782f0
KauanNascimentoSRosa <kauan.nascimento@unifesp.br>
2026-06-24 20:51:00 -0300

    jogo.cc

---
+++ jogo.cc
@@ -0,0 +1,115 @@
+#include <iostream>
+using namespace std;
+
+int V[9][9];
+
+int verificacao (int L, int C, int N){
+
+    int l, c, i, j;
+
+    for (j = 0; j < 9; j++){
+        if (j != C && V[L][j] == N){
+            return 0;
+        }
+    }
+
+    for (i = 0; i < 9; i++){
+        if (i != L && V[i][C] == N){
+            return 0;
+        }
+    }
+
+    l = L - (L % 3);
+    c = C - (C % 3);
+
+    for (i = l; i < l + 3; i++){
+        for (j = c; j < c + 3; j++){
+            if ((i != L || j != C) && V[i][j] == N){
+                return 0;
+            }
+        }
+    }
+
+    return 1;
+
+}
+
+int main()
+{
+
+    int L, C, N, i, j, v = 0; 
+    FILE *arquivo = fopen("input2.txt", "r+");
+
+    if (arquivo == NULL){
+        cout << "Erro ao abrir o arquivo.";
+        return 1;
+    }
+
+    for (i = 0; i < 9; i++){
+        for (j = 0; j < 9; j++){
+            fscanf(arquivo, "%d", &V[i][j]);
+        }
+    }
+    fclose (arquivo);
+
+    for (i = 0; i < 9; i++){
+        for (j = 0; j < 9; j++){
+            if (V[i][j] == 0){
+                v++;
+            }
+        }
+    }
+
+    cout << "---SUDOKU---\n";
+
+    while (v != 0){
+        
+        for (i = 0; i < 9; i++){
+            for (j = 0; j < 9; j++){
+                if (V[i][j] == 0){
+                    cout << "_ ";
+                } else {
+                    cout << V[i][j] << " ";
+                }
+                
+                if ((j + 1) % 3 == 0 && j != 8){
+                    cout << "| ";
+                }
+            }
+            cout << "\n";
+
+            if ((i + 1) % 3 == 0 && i != 8){
+                cout << "------|-------|------\n";
+            }
+        }
+
+        cout << "Digite a jogada:\n";
+        cout << "Linha (0-8): ";
+        cin >> L;
+
+        cout << "Coluna (0-8): ";
+        cin >> C;
+
+        cout << "Número (1-9): ";
+        cin >> N;
+
+
+
+        if (L < 0 || L > 8 || C < 0 || C > 8){
+            cout << "Posição inválida. Tente novamente.\n";
+            continue;
+        }
+
+        if (verificacao(L, C, N) == 1 && N >= 1 && N <= 9 && V[L][C] == 0){
+            cout << "Essa é uma jogada válida!" << endl;
+            V[L][C] = N;
+            v--;
+        } else {
+            cout << "Essa é uma jogada inválida. Tente novamente." << endl;
+        }
+    }
+
+    if (v == 0){
+        cout << "Você concluiu o Sudoku!" << endl;
+    }
+}
