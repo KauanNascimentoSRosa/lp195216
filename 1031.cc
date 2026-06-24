3b4adb9120bd50836e9ead7f286e6ab53dc3b066
KauanNascimentoSRosa <kauan.nascimento@unifesp.br>
2026-06-24 02:19:28 -0300

    1031.cc

---
+++ 1031.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int região(int R, int S){
+    int V[100], l = 1, C = R, i, P = 0;
+    
+    for (i = 0; i < C; i++){
+        V[i] = i + 1;
+    }
+    
+    for (i = 0; i < C - 1; i++){
+        V[i] = V[i + 1];
+    }
+    C--;
+    
+    while (C > 1){
+        
+        P = (P + S - 1) % C;
+        
+        for (i = P; i < C - 1; i++){
+            V[i] = V[i + 1];
+        }
+        
+        C--;
+        
+        if (P == C){
+            P = 0;
+        }
+    }
+    
+    if (V[0] == 13){
+        return S;
+    } else {
+        return região (R, S + 1);
+    }
+}
+
+
+int main (){
+    
+    int R;
+    while (cin >> R && R != 0){
+        cout << região(R, 1) << endl;
+    }
+}
